f985aa7262386b37ff9cc5b54747f1b615afc5f3
Charlie Winnard <ctwinnard@gmail.com>
2021-11-04 11:11:21 -0600

    calendar day layout

--- client/app/routes/calendar.js
+++ client/app/routes/calendar.js
@@ -1,6 +1,14 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service'
 
+const formatEvent = (event) => {
+  return {
+    startTime: event.start.split('T')[1],
+    duration: event.duration,
+    booked: false
+  }
+}
+
 export default class CalendarRoute extends Route {
     @service
     apolloService
@@ -10,13 +18,16 @@ export default class CalendarRoute extends Route {
     async model (params) {
       const allEvents = await this.apolloService.fetchAllEvents()
       const eventsByDate = {};
-      // group events by day
       allEvents.forEach(event => {
+          // format data for usability in template
+          const formattedEvent = formatEvent(event);
+
+          // group events by day
           const day = event.start.split('T')[0]
           if (Object.keys(eventsByDate).includes(day)) {
-            eventsByDate[day].push(event)
+            eventsByDate[day].push(formattedEvent)
           } else {
-            eventsByDate[day] = [event]
+            eventsByDate[day] = [formattedEvent]
           }
       })
       return eventsByDate
